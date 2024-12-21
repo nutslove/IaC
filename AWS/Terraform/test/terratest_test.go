62ac794397ee1afb3398d52d762eabd63485d117
Lee Joonki <nuts_love86@yahoo.co.jp>
2024-12-21 21:03:10 +0900

    test

---
+++ AWS/Terraform/test/terratest_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"github.com/gruntwork-io/terratest/modules/terraform"
+	"testing"
+)
+
+func TestTerratest(t *testing.T) {
+	t.Parallel()
+	awsRegion := "ap-northeast-1"
+	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
+		TerraformDir: "../AWS/Terraform/AWS/dev/tokyo/",
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
+	})
+	if _, err := terraform.InitE(t, terraformOptions); err != nil {
+		t.Errorf("Terraform Init Error: %v", err)
+		return
+	}
+	if _, err := terraform.PlanE(t, terraformOptions); err != nil {
+		t.Errorf("Terraform Plan Error: %v", err)
+		return
+	}
+	if _, err := terraform.ApplyE(t, terraformOptions); err != nil {
+		t.Errorf("Terraform Apply Error: %v", err)
+		terraform.Destroy(t, terraformOptions)
+	}
+}
