ea12937e130e08c300448b48d8ef74872303f156
Lee Joonki <nuts_love86@yahoo.co.jp>
2024-12-23 22:22:18 +0900

    module関連修正

--- AWS/Terraform/modules/iam/output.tf
+++ AWS/Terraform/modules/iam/output.tf
@@ -1,7 +1,7 @@
 output "test_policy_id" {
-  value = module.test.aws_iam_policy.test_policy.id
+  value = aws_iam_policy.test_policy.id
 }
 
 output "test_policy2_id" {
-  value = module.test.aws_iam_policy.test_policy2.id
+  value = aws_iam_policy.test_policy2.id
 }
