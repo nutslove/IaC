af5f8334b4557fde51789c3c1c89a9a14256c691
senaki <senaki@example.com>
2025-01-09 13:58:04 +0900

    modified

--- AWS/Terraform/AWS/dev/tokyo/main.tf
+++ AWS/Terraform/AWS/dev/tokyo/main.tf
@@ -10,8 +10,8 @@ module "vpc" {
 
   vpc_name = "senaki-vpc"
   senaki_vpc_cidr = "192.168.0.0/16"
-  senaki_vpc_subnet_a_cidr = "192.168.0.1/24"
-  senaki_vpc_subnet_c_cidr = "192.168.0.2/24"
+  senaki_vpc_subnet_a_cidr = "192.168.1.0/24"
+  senaki_vpc_subnet_c_cidr = "192.168.2.0/24"
   az_a = "ap-northeast-1a"
   az_c = "ap-northeast-1c"
 }
