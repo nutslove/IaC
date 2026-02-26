c7ae058beff98b2ac35cb4bcde5f3309df8fd446
Lee Joonki <nuts_love86@yahoo.co.jp>
2026-02-27 01:29:03 +0900

    modified

--- HomeLabs/Terraform/Modules/Grafana/Folders/outputs.tf
+++ HomeLabs/Terraform/Modules/Grafana/Folders/outputs.tf
@@ -2,28 +2,3 @@ output "k8s" {
   description = "k8s folder resource"
   value       = grafana_folder.k8s
 }
-
-output "loki" {
-  description = "Loki folder resource"
-  value       = grafana_folder.loki
-}
-
-output "thanos" {
-  description = "Thanos folder resource"
-  value       = grafana_folder.thanos
-}
-
-output "tempo" {
-  description = "Tempo folder resource"
-  value       = grafana_folder.tempo
-}
-
-output "langfuse" {
-  description = "Langfuse folder resource"
-  value       = grafana_folder.langfuse
-}
-
-output "argocd" {
-  description = "ArgoCD folder resource"
-  value       = grafana_folder.argocd
-}
